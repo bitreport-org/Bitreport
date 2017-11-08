0b8b37dbc00b1324ed606db31cc5e8c1062102c4
Tomek Urbaszek <turbaszek@gmail.com>
2017-11-08 18:22:54 +0100

    Change REST to Websockets

--- core/api_app.py
+++ core/api_app.py
@@ -4,15 +4,30 @@ from influxdb import InfluxDBClient
 
 app = Flask(__name__)
 api = Api(app)
-db = 'test'
 
-class GET_OHLC(Resource):
-    def get(self, db, pair, timeframe, limit):
-        # Connect to databse
-        client = InfluxDBClient('localhost', 8086, 'root', 'root', db)
+
+db = 'test3'
+client = InfluxDBClient('localhost', 8086, 'root', 'root', db)
+
+class get_ohlc(Resource):
+    def get(self, pair, timeframe, limit):
+        global client, db
 
         # Perform query and return JSON data
         query = 'SELECT * FROM ' + pair + timeframe + ' ORDER BY time DESC LIMIT ' + str(limit) + ';'
+        params = 'db=' + db + '&q=' + query
+        r =client.request('query', params=params)
+
+        # Unwrap json :D
+        return r.json()['results']#[0]['series'][0]['values']
+
+
+class get_pair(Resource):
+    def get(self, pair):
+        global client, db
+
+        # Perform query and return JSON data
+        query = 'SELECT * FROM ' + pair + ';'
         params = 'db=' + db + '&q=' + query
         r = client.request('query', params=params)
 
@@ -20,7 +35,8 @@ class GET_OHLC(Resource):
         return r.json()['results'][0]['series'][0]['values']
 
 
-api.add_resource(GET_OHLC, '/<string:db>/<string:pair>/<string:timeframe>/<int:limit>')
+api.add_resource(get_ohlc, '/candles/<string:pair>/<string:timeframe>/<int:limit>')
+api.add_resource(get_pair, '/<string:pair>')
 
 if __name__ == '__main__':
     app.run()
