2485a5fad614233630c55db75df7f1c2b1116575
Tomek Urbaszek <turbaszek@gmail.com>
2017-11-09 18:35:52 +0100

    Working API

--- core/services/api_app.py
+++ core/services/api_app.py
@@ -2,12 +2,18 @@ from flask import Flask
 from flask_restful import Resource, Api
 from influxdb import InfluxDBClient
 
+# Internal import
+from services import internal
+from ta import patterns
+
 app = Flask(__name__)
 api = Api(app)
 
-
-db = 'test3'
+# PARAMETERS
+db = 'test'
 client = InfluxDBClient('localhost', 8086, 'root', 'root', db)
+
+###############################################################
 
 class get_ohlc(Resource):
     def get(self, pair, timeframe, limit):
@@ -19,8 +25,7 @@ class get_ohlc(Resource):
         r =client.request('query', params=params)
 
         # Unwrap json :D
-        return r.json()['results']#[0]['series'][0]['values']
-
+        return r.json()['results'][0]['series'][0]['values']
 
 class get_pair(Resource):
     def get(self, pair):
@@ -34,9 +39,24 @@ class get_pair(Resource):
         # Unwrap json :D
         return r.json()['results'][0]['series'][0]['values']
 
+class get_patterns(Resource):
+    def get(self, pair, timeframe, limit ):
+        global client, db
 
-api.add_resource(get_ohlc, '/candles/<string:pair>/<string:timeframe>/<int:limit>')
+        data = internal.import_numpy(db, client, pair, timeframe, limit)
+        return patterns.CheckAllPatterns(data)
+
+##################### ENDPOINTS ############################
+# Table with name 'pair'
 api.add_resource(get_pair, '/<string:pair>')
 
+# List of last n=limit [close,  high,   low,    open,   volume] candles
+api.add_resource(get_ohlc, '/candles/<string:pair>/<string:timeframe>/<int:limit>')
+
+# Patterns {'pattern_name' : {'up' : [dates], 'down' : [dates]}, ...}
+api.add_resource(get_patterns, '/patterns/<string:pair>/<string:timeframe>/<int:limit>')
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
---
+++ core/services/dbfill.py
@@ -0,0 +1,36 @@
+import requests
+from influxdb import InfluxDBClient
+
+
+
+def bitfinex_fill(client, db, pair, timeframes, limit):
+    for timeframe in timeframes:
+        url = 'https://api.bitfinex.com/v2/candles/trade:' + timeframe + ':t' + pair + '/hist?limit=' + str(
+            limit) + '&start=946684800000'
+        candel_list = requests.get(url).json()
+        for i in range(len(candel_list)):
+            json_body = [
+                {
+                    "measurement": pair + timeframe,
+                    "time": int(1000000 * candel_list[i][0]),
+                    "fields": {
+                        "open": float(candel_list[i][1]),
+                        "close": float(candel_list[i][2]),
+                        "high": float(candel_list[i][3]),
+                        "low": float(candel_list[i][4]),
+                        "volume": float(candel_list[i][5]),
+                    }
+                }
+            ]
+            client.write_points(json_body)
+
+
+if __name__ == "__main__":
+    # PARAMETERS
+    db = 'test'
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', db)
+
+    pair = 'BTCUSD'
+    timeframes = ['30m', '1h', '3h', '12h']
+
+    bitfinex_fill(client, db, pair, timeframes, 400)
