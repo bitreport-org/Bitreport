444f559687d4193398849dc42c1b469b5a073a44
Tomek Urbaszek <turbaszek@gmail.com>
2018-01-08 16:43:15 +0100

    No data exception

--- core/core/services/internal.py
+++ core/core/services/internal.py
@@ -37,45 +37,48 @@ def import_numpy(pair, timeframe, limit):
     params = 'db=' + db + '&q=' + query
     r = client.request('query', params=params)
 
-    # Unwrap json :D
-    candel_list = r.json()['results'][0]['series'][0]['values']
+    # Unwrap json :
+    try:
+        candel_list = r.json()['results'][0]['series'][0]['values']
 
-    date_list=[]
-    open_list=[]
-    close_list=[]
-    high_list=[]
-    low_list=[]
-    volume_list=[]
+        date_list=[]
+        open_list=[]
+        close_list=[]
+        high_list=[]
+        low_list=[]
+        volume_list=[]
 
-    for i in range(0, len(candel_list)):
-        # change data to timestamp
-        t = candel_list[i][0]
-        dt = iso8601.parse_date(t)
-        dt = int(time.mktime(dt.timetuple()))
+        for i in range(0, len(candel_list)):
+            # change data to timestamp
+            t = candel_list[i][0]
+            dt = iso8601.parse_date(t)
+            dt = int(time.mktime(dt.timetuple()))
 
-        date_list.append(dt)
-        close_list.append(float(candel_list[i][1]))
-        high_list.append(float(candel_list[i][2]))
-        low_list.append(float(candel_list[i][3]))
-        open_list.append(float(candel_list[i][4]))
-        volume_list.append(float(candel_list[i][5]))
+            date_list.append(dt)
+            close_list.append(float(candel_list[i][1]))
+            high_list.append(float(candel_list[i][2]))
+            low_list.append(float(candel_list[i][3]))
+            open_list.append(float(candel_list[i][4]))
+            volume_list.append(float(candel_list[i][5]))
 
-    date_list.reverse()
-    close_list.reverse()
-    open_list.reverse()
-    high_list.reverse()
-    low_list.reverse()
-    volume_list.reverse()
+        date_list.reverse()
+        close_list.reverse()
+        open_list.reverse()
+        high_list.reverse()
+        low_list.reverse()
+        volume_list.reverse()
 
-    candles_dict = {'date' : date_list,
-                    'open' : np.array(open_list),
-                    'close' : np.array(close_list),
-                    'high' : np.array(high_list),
-                    'low' : np.array(low_list),
-                    'volume' : np.array(volume_list)
-                    }
+        candles_dict = {'date' : date_list,
+                        'open' : np.array(open_list),
+                        'close' : np.array(close_list),
+                        'high' : np.array(high_list),
+                        'low' : np.array(low_list),
+                        'volume' : np.array(volume_list)
+                        }
 
-    return candles_dict
+        return candles_dict
+    except:
+        return False
 
 
 def import_numpy_untill(pair, timeframe, limit, untill):
@@ -91,44 +94,47 @@ def import_numpy_untill(pair, timeframe, limit, untill):
     r = client.request('query', params=params)
 
     # Unwrap json :D
-    candel_list = r.json()['results'][0]['series'][0]['values']
+    try:
+        candel_list = r.json()['results'][0]['series'][0]['values']
 
-    date_list=[]
-    open_list=[]
-    close_list=[]
-    high_list=[]
-    low_list=[]
-    volume_list=[]
+        date_list=[]
+        open_list=[]
+        close_list=[]
+        high_list=[]
+        low_list=[]
+        volume_list=[]
 
-    for i in range(0, len(candel_list)):
-        # change data to timestamp
-        t = candel_list[i][0]
-        dt = iso8601.parse_date(t)
-        dt = int(time.mktime(dt.timetuple()))
+        for i in range(0, len(candel_list)):
+            # change data to timestamp
+            t = candel_list[i][0]
+            dt = iso8601.parse_date(t)
+            dt = int(time.mktime(dt.timetuple()))
 
-        date_list.append(dt)
-        close_list.append(float(candel_list[i][1]))
-        high_list.append(float(candel_list[i][2]))
-        low_list.append(float(candel_list[i][3]))
-        open_list.append(float(candel_list[i][4]))
-        volume_list.append(float(candel_list[i][5]))
+            date_list.append(dt)
+            close_list.append(float(candel_list[i][1]))
+            high_list.append(float(candel_list[i][2]))
+            low_list.append(float(candel_list[i][3]))
+            open_list.append(float(candel_list[i][4]))
+            volume_list.append(float(candel_list[i][5]))
 
-    date_list.reverse()
-    close_list.reverse()
-    open_list.reverse()
-    high_list.reverse()
-    low_list.reverse()
-    volume_list.reverse()
+        date_list.reverse()
+        close_list.reverse()
+        open_list.reverse()
+        high_list.reverse()
+        low_list.reverse()
+        volume_list.reverse()
 
-    candles_dict = {'date' : date_list,
-                    'open' : np.array(open_list),
-                    'close' : np.array(close_list),
-                    'high' : np.array(high_list),
-                    'low' : np.array(low_list),
-                    'volume' : np.array(volume_list)
-                    }
+        candles_dict = {'date' : date_list,
+                        'open' : np.array(open_list),
+                        'close' : np.array(close_list),
+                        'high' : np.array(high_list),
+                        'low' : np.array(low_list),
+                        'volume' : np.array(volume_list)
+                        }
 
-    return candles_dict
+        return candles_dict
+    except:
+        return False
 
 
 def generate_dates(data, timeframe, margin):
