8ef04c66ab6c36591779e71cfed520771e86da9d
Tomek Urbaszek <turbaszek@gmail.com>
2018-04-14 16:46:54 +0200

    Use numpy and pandas to return data

--- core/core/services/internal.py
+++ core/core/services/internal.py
@@ -1,18 +1,15 @@
 import numpy as np
-import time
-import iso8601
 import configparser
 from influxdb import InfluxDBClient
-import math, decimal, datetime
+import math
+import datetime
+from decimal import Decimal as dec
 import psycopg2
-
-dec = decimal.Decimal
+import pandas as pd
 
 def Config(file, section):
-
     config = configparser.ConfigParser()
     config.read(file)
-    #print(Config.sections())
 
     dict1 = {}
     options = config.options(section)
@@ -35,47 +32,20 @@ def import_numpy(pair, timeframe, limit):
     client = InfluxDBClient(host, port, 'root', 'root', db)
 
     # Perform query and return JSON data
-    query = 'SELECT * FROM ' + pair + timeframe + ' ORDER BY time DESC LIMIT ' + str(limit) + ';'
-    params = 'db=' + db + '&q=' + query
-    r = client.request('query', params=params)
-
-    # Unwrap json :
+    query = 'SELECT * FROM {} ORDER BY time DESC LIMIT {};'.format(pair+timeframe, limit)
+    params = 'db={}&q={}&epoch=s'.format(db, query)
+    r = client.request('query', params=params).json()['results'][0]['series'][0]
     try:
-        candel_list = r.json()['results'][0]['series'][0]['values']
+        candle_list = r['values']
+        candle_list.reverse()
+        df = pd.DataFrame(candle_list, columns=r['columns'])
 
-        date_list=[]
-        open_list=[]
-        close_list=[]
-        high_list=[]
-        low_list=[]
-        volume_list=[]
-
-        for i in range(0, len(candel_list)):
-            # change data to timestamp
-            t = candel_list[i][0]
-            dt = iso8601.parse_date(t)
-            dt = int(time.mktime(dt.timetuple()))
-
-            date_list.append(dt)
-            close_list.append(float(candel_list[i][1]))
-            high_list.append(float(candel_list[i][2]))
-            low_list.append(float(candel_list[i][3]))
-            open_list.append(float(candel_list[i][4]))
-            volume_list.append(float(candel_list[i][5]))
-
-        date_list.reverse()
-        close_list.reverse()
-        open_list.reverse()
-        high_list.reverse()
-        low_list.reverse()
-        volume_list.reverse()
-
-        candles_dict = {'date' : date_list,
-                        'open' : np.array(open_list),
-                        'close' : np.array(close_list),
-                        'high' : np.array(high_list),
-                        'low' : np.array(low_list),
-                        'volume' : np.array(volume_list)
+        candles_dict = {'date': list(df.time),
+                        'open': np.array(df.open),
+                        'close': np.array(df.close),
+                        'high': np.array(df.high),
+                        'low': np.array(df.low),
+                        'volume': np.array(df.volume)
                         }
 
         return candles_dict
@@ -91,47 +61,20 @@ def import_numpy_untill(pair, timeframe, limit, untill):
     client = InfluxDBClient(host, port, 'root', 'root', db)
 
     # Perform query and return JSON data
-    query = 'SELECT * FROM ' + pair + timeframe + ' WHERE time<=' + str(1000000000*untill) +' ORDER BY time DESC LIMIT ' + str(limit) + ';'
-    params = 'db=' + db + '&q=' + query
-    r = client.request('query', params=params)
-
-    # Unwrap json :D
+    query = 'SELECT * FROM {} WHERE time<={} ORDER BY time DESC LIMIT {};'.format(pair+timeframe, 1000000000*untill, limit)
+    params = 'db={}&q={}&epoch=s'.format(db, query)
+    r = client.request('query', params=params).json()['results'][0]['series'][0]
     try:
-        candel_list = r.json()['results'][0]['series'][0]['values']
+        candle_list = r['values']
+        candle_list.reverse()
+        df = pd.DataFrame(candle_list, columns=r['columns'])
 
-        date_list=[]
-        open_list=[]
-        close_list=[]
-        high_list=[]
-        low_list=[]
-        volume_list=[]
-
-        for i in range(0, len(candel_list)):
-            # change data to timestamp
-            t = candel_list[i][0]
-            dt = iso8601.parse_date(t)
-            dt = int(time.mktime(dt.timetuple()))
-
-            date_list.append(dt)
-            close_list.append(float(candel_list[i][1]))
-            high_list.append(float(candel_list[i][2]))
-            low_list.append(float(candel_list[i][3]))
-            open_list.append(float(candel_list[i][4]))
-            volume_list.append(float(candel_list[i][5]))
-
-        date_list.reverse()
-        close_list.reverse()
-        open_list.reverse()
-        high_list.reverse()
-        low_list.reverse()
-        volume_list.reverse()
-
-        candles_dict = {'date' : date_list,
-                        'open' : np.array(open_list),
-                        'close' : np.array(close_list),
-                        'high' : np.array(high_list),
-                        'low' : np.array(low_list),
-                        'volume' : np.array(volume_list)
+        candles_dict = {'date': list(df.time),
+                        'open': np.array(df.open),
+                        'close': np.array(df.close),
+                        'high': np.array(df.high),
+                        'low': np.array(df.low),
+                        'volume': np.array(df.volume)
                         }
 
         return candles_dict
@@ -143,8 +86,7 @@ def generate_dates(data, timeframe, margin):
     # Generate timestamps for future
     date = data['date']
     period = timeframe[-1]
-    timef = timeframe[:-1]
-    t = int(timef)
+    t = int(timeframe[:-1])
 
     d = 0
     if period == 'm':
@@ -210,10 +152,8 @@ def what_phase(timestamp):
 def show_pairs():
     conf = Config('config.ini', 'services')
     db = conf['postgre_db']
-    user = conf['postgre_user']
 
-    q = "dbname='" + db + "' host='localhost' "
-    conn = psycopg2.connect(q)
+    conn = psycopg2.connect(dbname=db, host='localhost')
     cur = conn.cursor()
 
     SQL = "SELECT * FROM available_pairs;"
@@ -233,7 +173,6 @@ def show_pairs():
 def add_pair(pair, exchange):
     conf = Config('config.ini', 'services')
     db = conf['postgre_db']
-    user = conf['postgre_user']
 
     q = "dbname='"+db+"' host='localhost' "
     conn = psycopg2.connect(q)
@@ -243,6 +182,7 @@ def add_pair(pair, exchange):
     SQL = "SELECT * FROM available_pairs WHERE pair=%s and exchange=%s"
     cur.execute(SQL, data,)
     rows=cur.fetchall()
+
     if len(rows) == 0:
         SQL = "INSERT INTO available_pairs VALUES (%s, %s);"
         cur.execute(SQL, data,)
@@ -259,7 +199,6 @@ def add_pair(pair, exchange):
 def check_exchange(pair):
     conf = Config('config.ini', 'services')
     db = conf['postgre_db']
-    user = conf['postgre_user']
 
     q = "dbname='" + db + "' host='localhost' "
     conn = psycopg2.connect(q)
