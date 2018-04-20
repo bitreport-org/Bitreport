b5f71f240323df08e068a12cc1168633b4a703db
Tomek Urbaszek <turbaszek@gmail.com>
2018-04-20 23:18:23 +0200

    Api based on flask routing

--- core/core/core.py
+++ core/core/core.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, request, jsonify
 import ast
 import time
 import datetime
@@ -14,7 +13,6 @@ from ta import indicators, levels, patterns, channels
 import config
 
 app = Flask(__name__)
-api = Api(app)
 
 # Config
 conf = config.BaseConfig()
@@ -35,169 +33,144 @@ app.logger.addHandler(handler)
 def hello():
     return "Wrong place, is it?"
 
-
 #### API ####
 
 # to post data without NaN values indicators are calculated on period of length: limit + magic_limit
-# next posted data has length = limit
+# returned data has length = limit
 magic_limit = conf.MAGIC_LIMIT
 
+@app.route('/data/<pair>/<timeframe>/', methods=['GET'])
+def data_service(pair, timeframe):
+    limit = request.args.get('limit', default=None, type=int)
+    untill = request.args.get('untill', default=None, type=int)
+    print(limit, untill)
 
-class All(Resource):
-    def get(self, pair, timeframe):
-        tic = time.time()
-        ################################## PARSER #######################################
-        parser = reqparse.RequestParser()
+    tic = time.time()
+    ############################### DATA REQUEST #####################################
 
-        parser.add_argument('limit', type=int, help='Limit must be int')
-        parser.add_argument('untill', type=int, help='Limit must be int')
-        args = parser.parse_args()
+    output = {}
 
-        limit = args.get('limit')
-        untill = args.get('untill')
-        ############################### DATA REQUEST #####################################
+    # TODO request data always with untill parameter
+    if isinstance(untill, int):
+        data = internal.import_numpy_untill(pair, timeframe, limit + magic_limit, untill)
+    else:
+        data = internal.import_numpy(pair, timeframe, limit + magic_limit)
 
-        output = {}
+    if data == False:
+        return 'Error', 500
 
-        #TODO request data always with untill parameter
-        if isinstance(untill, int):
-            data = internal.import_numpy_untill(pair, timeframe, limit + magic_limit, untill)
-        else:
-            data = internal.import_numpy(pair, timeframe, limit + magic_limit)
+    # SET margin
+    margin = 26  # timestamps
 
-        if data == False:
-            return 'Error', 500
+    # Generate timestamps for future
+    dates = internal.generate_dates(data, timeframe, margin)
+    output['dates'] = dates[magic_limit:]
 
-        # SET margin
-        margin = 26  #timestamps
+    output['candles'] = {'open': data['open'].tolist()[magic_limit:],
+                         'high': data['high'].tolist()[magic_limit:],
+                         'close': data['close'].tolist()[magic_limit:],
+                         'low': data['low'].tolist()[magic_limit:],
+                         'volume': data['volume'].tolist()[magic_limit:]
+                         }
+    ################################ INDICATORS ######################################
 
-        # Generate timestamps for future
-        dates = internal.generate_dates(data, timeframe, margin)
-        output['dates'] = dates[magic_limit:]
-
-        output['candles'] = { 'open': data['open'].tolist()[magic_limit:],
-                            'high': data['high'].tolist()[magic_limit:],
-                            'close': data['close'].tolist()[magic_limit:],
-                            'low': data['low'].tolist()[magic_limit:],
-                            'volume': data['volume'].tolist()[magic_limit:]
-                            }
-        ################################ INDICATORS ######################################
-
-        indicators_list = internal.get_function_list(indicators)
-        indidict = {}
-        for indic in indicators_list:
-            try:
-                indidict[indic] = getattr(indicators, indic)(data, start=magic_limit)
-            except Exception as e:
-                app.logger.warning(indic)
-                app.logger.warning(traceback.format_exc())
-                pass
-
-        ################################ CHANNELS #########################################
-        #TODO after channels implementation in Dashboard it must be adjusted
-        # Basic channels
+    indicators_list = internal.get_function_list(indicators)
+    indidict = {}
+    for indic in indicators_list:
         try:
-            indidict['channel'] = channels.channel(data, start=magic_limit)
-        except:
-            pass
-
-        try:
-            indidict['parabola'] = channels.parabola(data, start=magic_limit)
-        except:
-            pass
-
-        try:
-            indidict['wedge'] = channels.fallingwedge(data, start=magic_limit)
-        except:
-            pass
-
-        output['indicators'] = indidict
-
-        ################################ PATTERNS ########################################
-        # Short data for patterns
-        if isinstance(untill, int):
-            pat_data = internal.import_numpy_untill(pair, timeframe, limit + magic_limit, untill)
-        else:
-            pat_data = internal.import_numpy(pair, timeframe, limit + magic_limit)
-
-        try:
-            output['patterns'] = patterns.CheckAllPatterns(pat_data)
+            indidict[indic] = getattr(indicators, indic)(data, start=magic_limit)
         except Exception as e:
+            app.logger.warning(indic)
             app.logger.warning(traceback.format_exc())
-            output['patterns']=[]
             pass
 
-        ################################ LEVELS ##########################################
-        try:
-            output['levels'] = levels.srlevels(data)
-        except Exception as e:
-            app.logger.warning(traceback.format_exc())
-            output['levels'] = []
-            pass
-        toc = time.time()
-        output['response_time'] = '{0:.2f} s'.format(toc-tic)
-        return output, 200
+    ################################ CHANNELS #########################################
+    # TODO after channels implementation in Dashboard it must be adjusted
+    # Basic channels
+    try:
+        indidict['channel'] = channels.channel(data, start=magic_limit)
+    except:
+        pass
+
+    try:
+        indidict['parabola'] = channels.parabola(data, start=magic_limit)
+    except:
+        pass
+
+    try:
+        indidict['wedge'] = channels.fallingwedge(data, start=magic_limit)
+    except:
+        pass
+
+    output['indicators'] = indidict
+
+    ################################ PATTERNS ########################################
+    # Short data for patterns
+    if isinstance(untill, int):
+        pat_data = internal.import_numpy_untill(pair, timeframe, limit + magic_limit, untill)
+    else:
+        pat_data = internal.import_numpy(pair, timeframe, limit + magic_limit)
+
+    try:
+        output['patterns'] = patterns.CheckAllPatterns(pat_data)
+    except Exception as e:
+        app.logger.warning(traceback.format_exc())
+        output['patterns'] = []
+        pass
+
+    ################################ LEVELS ##########################################
+    try:
+        output['levels'] = levels.srlevels(data)
+    except Exception as e:
+        app.logger.warning(traceback.format_exc())
+        output['levels'] = []
+        pass
+    toc = time.time()
+    output['response_time'] = '{0:.2f} s'.format(toc - tic)
+    return jsonify(output), 200
 
 
 events_list = []
-class Events(Resource):
-    def get(self):
-        return events_list
-
-    def put(self):
+@app.route('/events', methods=['GET', 'PUT'])
+def event_servie():
+    if request.method == 'PUT':
         events_list.append(ast.literal_eval(request.form['data']))
         now = int(time.mktime(datetime.datetime.now().timetuple()))
         for event in events_list:
-            if now - event['time'] > 60*int(conf['event_limit']):
+            if now - event['time'] > 60 * int(conf['event_limit']):
                 events_list.pop(events_list.index(event))
+    elif request.method == 'GET':
+        return jsonify(events_list)
 
 
-class Fill(Resource):
-    def post(self, pair):
-        exchange = internal.check_exchange(pair)
-        parser = reqparse.RequestParser()
-        parser.add_argument('last', type=int)
-        args = parser.parse_args()
-        last = args.get('last')
-        if exchange != 'None':
-            try:
-                return dbservice.pair_fill(app, pair, exchange, last)
-            except:
-                app.logger.warning(traceback.format_exc())
-                return 'Request failed', 500
-        else:
-            return 500
+@app.route('/fill/<pair>', methods=['POST'])
+def fill_service(pair):
+    last = request.args.get('last', type=int)
+    exchange = internal.check_exchange(pair)
+    if exchange != 'None':
+        try:
+            return dbservice.pair_fill(app, pair, exchange, last)
+        except:
+            app.logger.warning(traceback.format_exc())
+            return 'Request failed', 500
+    else:
+        return 'Pair not added'
 
 
-class Pairs(Resource):
-    def get(self):
+@app.route('/pairs', methods=['GET', 'POST'])
+def pair_service():
+    if request.method == 'GET':
         try:
             pairs_list = internal.show_pairs()
-            return pairs_list
+            return jsonify(pairs_list)
         except:
             return 'Shit!', 500
-
-    def post(self):
+    elif request.method == 'POST':
         try:
-            parser = reqparse.RequestParser()
-            parser.add_argument('pair')
-            parser.add_argument('exchange')
-            args = parser.parse_args()
-            exchange = args.get('exchange')
-            pair = args.get('pair')
+            exchange = request.args.get('exchange', type=str)
+            pair = request.args.get('pair', type=str)
             response = internal.add_pair(pair, exchange)
-            return response
-
+            return jsonify(response)
         except:
-            return 'Shit!', 500
-
-
-##################### ENDPOINTS ############################
-# Table with name 'pair'
-api.add_resource(All, '/data/<string:pair>/<string:timeframe>/')
-api.add_resource(Events, '/events')
-api.add_resource(Fill, '/fill/<string:pair>')
-api.add_resource(Pairs, '/pairs')
-#api.add_resource(Channels, '/channel/<string:pair>/<string:timeframe>')
-
-
+            app.logger.warning(traceback.format_exc())
+            return 'Request failed', 500
