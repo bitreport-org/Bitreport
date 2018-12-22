46eb2e6d58f4e8896b7349a3e6dd15882e05bc76
Maciej Głowacki <bombasarkadian@gmail.com>
2018-12-22 14:09:45 +0100

    Add error reporting

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,56 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_103_183_830) do
+ActiveRecord::Schema.define(version: 2018_11_03_183830) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'pairs', force: :cascade do |t|
-    t.string 'symbol', null: false
-    t.string 'name'
-    t.string 'exchange'
-    t.datetime 'last_updated_at'
-    t.string 'tags', null: false, array: true
-    t.index ['symbol'], name: 'index_pairs_on_symbol'
+  create_table "pairs", force: :cascade do |t|
+    t.string "symbol", null: false
+    t.string "name"
+    t.string "exchange"
+    t.datetime "last_updated_at"
+    t.string "tags", null: false, array: true
+    t.index ["symbol"], name: "index_pairs_on_symbol"
   end
 
-  create_table 'push_devices', force: :cascade do |t|
-    t.string 'endpoint', null: false
-    t.string 'p256dh'
-    t.string 'auth'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['endpoint'], name: 'index_push_devices_on_endpoint'
+  create_table "push_devices", force: :cascade do |t|
+    t.string "endpoint", null: false
+    t.string "p256dh"
+    t.string "auth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["endpoint"], name: "index_push_devices_on_endpoint"
   end
 
-  create_table 'twitter_images', force: :cascade do |t|
-    t.string 'symbol', null: false
-    t.string 'timeframe', null: false
-    t.integer 'limit'
-    t.string 'indicators', array: true
-    t.string 'levels', array: true
-    t.string 'patterns', array: true
-    t.text 'comment'
-    t.text 'image_data'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'pair_id'
-    t.datetime 'published_at'
-    t.string 'media_id'
-    t.index ['pair_id'], name: 'index_twitter_images_on_pair_id'
+  create_table "twitter_images", force: :cascade do |t|
+    t.string "symbol", null: false
+    t.string "timeframe", null: false
+    t.integer "limit"
+    t.string "indicators", array: true
+    t.string "levels", array: true
+    t.string "patterns", array: true
+    t.text "comment"
+    t.text "image_data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "pair_id"
+    t.datetime "published_at"
+    t.string "media_id"
+    t.index ["pair_id"], name: "index_twitter_images_on_pair_id"
   end
 
-  create_table 'twitter_responses', force: :cascade do |t|
-    t.bigint 'twitter_image_id'
-    t.string 'in_reply_to'
-    t.index ['twitter_image_id'], name: 'index_twitter_responses_on_twitter_image_id'
+  create_table "twitter_responses", force: :cascade do |t|
+    t.bigint "twitter_image_id"
+    t.string "in_reply_to"
+    t.index ["twitter_image_id"], name: "index_twitter_responses_on_twitter_image_id"
   end
 
-  create_table 'wallets', force: :cascade do |t|
-    t.boolean 'used', default: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "wallets", force: :cascade do |t|
+    t.boolean "used", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'twitter_responses', 'twitter_images'
+  add_foreign_key "twitter_responses", "twitter_images"
 end
