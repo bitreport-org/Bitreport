c50fcce6ae6bbb8268f6ce0e6499f29e2d08cc9b
nuclearpinguin <turbaszek@gmail.com>
2018-12-31 15:24:58 +0100

    Logger, sentry and environment setup

--- core/config.py
+++ core/config.py
@@ -1,4 +1,7 @@
+import os
+
 class BaseConfig(object):
+    PROD = int(os.environ.get("PRODUCTION", '0'))
     DBNAME = 'pairs'
     HOST = 'influx'
     PORT = 8086
@@ -9,13 +12,18 @@ class BaseConfig(object):
     POSTGRES_DATABSE = 'core'
     POSTGRES_USER = 'postgres'
     CHART_TABLE = 'charting'
-
-
-# class BaseConfig(object):
-#     DBNAME = 'test'
-#     HOST = 'localhost'
-#     PORT = 8086
-#     MAGIC_LIMIT = 79
-#     EVENT_LIMIT = 3
-#     EXCHANGES = 'exchanges.npz'
-#     MARGIN=26
+    LOGGER = {
+                'version': 1,
+                'formatters': {'default': {
+                    'format': '[%(asctime)s] - core - %(levelname)s : %(message)s',
+                }},
+                'handlers': {'wsgi': {
+                    'class': 'logging.StreamHandler',
+                    'stream': 'ext://flask.logging.wsgi_errors_stream',
+                    'formatter': 'default'
+                }},
+                'root': {
+                    'level': 'INFO',
+                    'handlers': ['wsgi']
+                }
+            }
