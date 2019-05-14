fix: errors in charting 📈 - double patterns, angle calculation 🤯 - triangles, band is an array not a float 💁🏻‍♂️ - levels, return all levels not only the last iteration 🙈

core/app/ta/charting/constructors.py
@@ -22,7 +22,7 @@ def _is_peak(xs: np.ndarray, ts: np.ndarray, checker: callable) -> List[Point]:
     list of peaks
     """
     lines = []
-    radius = 10
+    radius = 7
 
     # For each strength level
     for i, (y, t) in enumerate(zip(xs, ts)):

core/app/ta/levels/checker.py
@@ -5,7 +5,7 @@ from functools import reduce
 LevelTuple = namedtuple('LevelTuple', ['type', 'value', 'time', 'strength'])
 
 
-def is_level(i: int, x: float, xs: np.ndarray, t: int) -> tuple:
+def is_level(i: int, x: float, xs: np.ndarray, t: int) -> [LevelTuple]:
     """
     Checks if value x is a significant level in series of xs.
     Parameters
@@ -25,7 +25,7 @@ def is_level(i: int, x: float, xs: np.ndarray, t: int) -> tuple:
         1: 10
     }
 
-    lvl = None
+    lvls = []
 
     # For each strength level
     for strength, radius in _radius_map.items():
@@ -38,7 +38,9 @@ def is_level(i: int, x: float, xs: np.ndarray, t: int) -> tuple:
         resistance = reduce(lambda a, b: a and b, ys <= np.array([x] * ys.size))
         if support:
             lvl = LevelTuple('support', x, int(t), strength)
+            lvls.append(lvl)
         elif resistance:
             lvl = LevelTuple('resistance', x, int(t), strength)
+            lvls.append(lvl)
 
-    return lvl
+    return lvls

core/app/ta/levels/levels.py
@@ -5,6 +5,7 @@ from app.ta.levels.checker import is_level
 from app.ta.levels.fuzzy_level import FuzzyLevel
 from app.ta.charting.base import Universe
 
+from functools import reduce
 
 class Levels(object):
     def __init__(self, universe: Universe) -> None:
@@ -52,8 +53,9 @@ class Levels(object):
         close - time series of price values
         """
         lvls = [is_level(i, x, close, time[i]) for i, x in enumerate(close)]
-        lvls = list(filter(lambda x: x, lvls))
-        self._save_levels(lvls)
+        lvls = reduce(lambda a, b: a + b, lvls)
+        if lvls:
+            self._save_levels(lvls)
 
     def _score_levels(self, close: np.ndarray, r: float = 0.03) -> list:
         """

core/app/ta/patterns/double.py
@@ -13,12 +13,18 @@ def _angle(a: Point, b: Point, c: Point) -> float:
     """
 
     A = [a.x - b.x, a.y - b.y]
-    B = [c.x - b.x, c.y - c.y]
+    B = [c.x - b.x, c.y - b.y]
+    C = [c.x - a.x, c.y - a.y]
 
     lA = np.sqrt(A[0] ** 2 + A[1] ** 2)
     lB = np.sqrt(B[0] ** 2 + B[1] ** 2)
+    lC = np.sqrt(C[0] ** 2 + C[1] ** 2)
 
-    alpha = np.degrees(np.arccos(np.dot(A, B) / (lA * lB)))
+    d = (lC ** 2 - lA ** 2 - lB ** 2) / (-2 * lA * lB)
+
+    print(d)
+
+    alpha = np.degrees(np.arccos(d))
     return float(alpha)
 
 
@@ -85,34 +91,33 @@ def make_double(universe: Universe,
 
     slopes.sort(key=lambda x: abs(x[1]))
 
-    bx = slopes[0][0]
-    B = Point(int(bx), float(close[bx]))
+    cx = slopes[0][0]
+    C = Point(int(cx), float(close[cx]))
 
-    if (B.x - A.x) > 0.3 * close.size:
+    if (C.x - A.x) > 0.3 * close.size:
         return {'info': []}
 
     # Find the midpoint
-    cx = A.x + g(close[A.x: B.x])
-    C = Point(int(cx), float(close[cx]))
+    bx = A.x + g(close[A.x: C.x])
+    B = Point(int(bx), float(close[bx]))
 
     # Scale 3 points to [0,1]x[0,1]
-    xs = np.array([A.x, B.x, C.x])
-    scaled_x = (xs - B.x) / (A.x - B.x)
+    xs = np.array([A.x, C.x, B.x])
+    scaled_x = (xs - C.x) / (A.x - C.x)
 
-    ys = np.array([A.y, B.y, C.y])
+    ys = np.array([A.y, C.y, B.y])
 
     if type_ == 'top':
-        scaled_y = (ys - B.y) / (A.y - B.y)
+        scaled_y = (ys - C.y) / (A.y - C.y)
     else:
-        scaled_y = (ys - A.y) / (B.y - A.y)
+        scaled_y = (ys - A.y) / (C.y - A.y)
 
-    sA, sB, sC = [Point(x, y) for x, y in zip(scaled_x, scaled_y)]
-    alpha = _angle(sA, sB, sC)
+    scaled_a, scaled_b, scaled_c = [Point(x, y) for x, y in zip(scaled_x, scaled_y)]
+    alpha = _angle(scaled_a, scaled_b, scaled_c)
     if alpha > 95:
         return {'info': [], 'A': (), 'B': (), 'C': ()}
 
-    # Here we change points order from A, C, B to A, B, C
-    dt = {k: (int(x_dates[p.x]), p.y) for k, p in zip('ACB', [A, B, C])}
+    dt = {k: (int(x_dates[p.x]), p.y) for k, p in zip('ABC', [A, B, C])}
 
     dt['info'] = []
 