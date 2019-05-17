09629bad2e88bcd0b9d46f037521567441ca3a85
nuclearpinguin <turbaszek@gmail.com>
2019-05-17 17:51:19 +0200

    feat: skips flat bottoms and tops

--- core/app/ta/patterns/double.py
+++ core/app/ta/patterns/double.py
@@ -55,7 +55,7 @@ def make_double(universe: Universe,
     -------
     dt: dictionary with params A, B, C which represents the following points in the pattern
     """
-    # TODO: assert that middle point is at least at some height?
+    DEAFULT = {'info': [], 'A': (), 'B': (), 'C': ()}
 
     x_dates = universe.time
     close = universe.close
@@ -71,7 +71,7 @@ def make_double(universe: Universe,
     A = Point(int(x), float(close[x]))
 
     if A.x > close.size - right_margin:
-        return {'info': [], 'A': (), 'B': (), 'C': ()}
+        return DEAFULT
 
     # Scale series from Ax to a square [0,1]x[0,1]
     scaled_y = close[A.x+1:]
@@ -84,7 +84,7 @@ def make_double(universe: Universe,
     scaled_x = scaled_x[threshold:]
 
     if scaled_x.size < 1:
-        return {'info': [], 'A': (), 'B': (), 'C': ()}
+        return DEAFULT
 
     # Calculate slopes
     slopes = (scaled_y[1:] - sgn) / scaled_x[1:]
@@ -96,7 +96,7 @@ def make_double(universe: Universe,
 
     # Select only points that are dist points from A
     if not slopes:
-        return {'info': [], 'A': (), 'B': (), 'C': ()}
+        return DEAFULT
 
     slopes.sort(key=lambda x: abs(x[1]))
 
@@ -104,27 +104,33 @@ def make_double(universe: Universe,
     C = Point(int(cx), float(close[cx]))
 
     if (C.x - A.x) > 0.3 * close.size or C.x - A.x < 10:
-        return {'info': [], 'A': (), 'B': (), 'C': ()}
+        return DEAFULT
 
     # Find the midpoint
     bx = A.x + g(close[A.x: C.x])
     B = Point(int(bx), float(close[bx]))
 
-    # Scale 3 points to [0,1]x[0,1]
-    xs = np.array([A.x, C.x, B.x])
-    scaled_x = (xs - C.x) / (A.x - C.x)
+    # Assert that the pattern is not flat
+    h1, h2 = abs(A.y / B.y - 1), abs(B.y / C.y - 1)
+    if h1 < 0.02 or h2 < 0.02:
+        return DEAFULT
 
-    ys = np.array([A.y, C.y, B.y])
+    # Scale 3 points to [0,1]x[0,1]
+    xs = np.array([A.x, B.x, C.x])
+    ys = np.array([A.y, B.y, C.y])
+
+    scaled_x = (xs - A.x) / (C.x - A.x)
 
     if type_ == 'top':
-        scaled_y = (ys - C.y) / (A.y - C.y)
+        scaled_y = (ys - B.y) / (A.y - B.y)
     else:
-        scaled_y = (ys - A.y) / (C.y - A.y)
+        scaled_y = (ys - A.y) / (B.y - A.y)
 
+    # The rocket must be pointy!
     scaled_a, scaled_b, scaled_c = [Point(x, y) for x, y in zip(scaled_x, scaled_y)]
     alpha = _angle(scaled_a, scaled_b, scaled_c)
     if alpha > 95:
-        return {'info': [], 'A': (), 'B': (), 'C': ()}
+        return DEAFULT
 
     dt = {k: (int(x_dates[p.x]), p.y) for k, p in zip('ABC', [A, B, C])}
 
